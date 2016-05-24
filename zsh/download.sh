6f058dbdf0d7b5b543740d6ba089c910ed7b0781
Breno Leitao <breno.leitao@gmail.com>
2016-05-23 22:41:21 -0300

    Set the default editor also

--- zsh/download.sh
+++ zsh/download.sh
@@ -4,3 +4,4 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 cp breno.zsh-theme ~/.oh-my-zsh/themes
 git config --global user.email breno.leitao@gmail.com
 git config --global user.name Breno Leitao
+git config --global core.editor vim
