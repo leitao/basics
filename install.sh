416206f3e9f5878cfadd8c54b95769991ab07618
Breno Leitao <breno.leitao@gmail.com>
2016-08-03 18:03:09 -0500

    more basic configurations

--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@
 
 cp basics/vimrc ~/.vimrc
 cp basics/zshrc ~/.zshrc
+
+sudo cp apt/* /etc/apt/apt.conf.d/
+
+apt-get install rsync git zsh cmus mutt abook openssh-server vim qemu qemu-kvm virtinst youtube-dl vim-syntastic iotop nethogs
