74e7a579d1ea992e06666eae9fa705b21cf5a1f3
Breno Leitao <brenohl@br.ibm.com>
2016-08-26 18:39:44 -0300

    adding git information

--- install.sh
+++ install.sh
@@ -5,4 +5,10 @@ cp basics/zshrc ~/.zshrc
 
 sudo cp apt/* /etc/apt/apt.conf.d/
 
-apt-get install rsync git zsh cmus mutt abook openssh-server vim qemu qemu-kvm virtinst youtube-dl vim-syntastic iotop nethogs
+apt-get install rsync git zsh cmus mutt abook openssh-server vim qemu qemu-kvm virtinst youtube-dl vim-syntastic iotop nethogs urlscan urlscan
+
+## git
+git config --global user.name "Breno Leitao"
+git config --global user.email brenohl@br.ibm.com
+git config --global core.editor vim
+git config --global --add gitreview.username "leitao"
