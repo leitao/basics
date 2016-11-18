0f05acfeca7b201870c101ce1feacd7f20424d28
Breno Leitao <breno.leitao@gmail.com>
2016-11-18 17:52:21 -0500

    New stuff

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 sudo cp apt/* /etc/apt/apt.conf.d/
 
-apt-get install rsync git zsh cmus mutt abook openssh-server vim qemu qemu-kvm virtinst youtube-dl vim-syntastic iotop nethogs urlscan urlscan terminator fonts-liberation
+apt-get install rsync git zsh cmus mutt abook openssh-server vim qemu qemu-kvm virtinst youtube-dl vim-syntastic iotop nethogs urlscan urlscan terminator fonts-liberation dpkg-dev curl vlc
 
 ## git
 git config --global user.name "Breno Leitao"
@@ -12,10 +12,8 @@ git config --global core.editor vim
 git config --global --add gitreview.username "leitao"
 git config --global credential.helper cache
 
-
 # zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-# post zsh
-
+# post zsh installation
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
